ffa090278b6e3593555e1f9469408935f6d5d123
Michael Zekoff <zekoff@gmail.com>
2023-02-11 11:59:39 -0600

    Add DEV mode banner

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CssBaseline />
+    {process.env.NODE_ENV === 'development' ? <p>[DEV]</p> : null}
     <App />
   </React.StrictMode>
 );
